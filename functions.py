e8bbb24f949192c6683bdfdb15e6914d55ed8633
Arsalan Farooqui <35590988+4rsalan@users.noreply.github.com>
2019-02-26 13:33:09 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,3 +1,6 @@
+import pymongo
+myclient = pymongo.MongoClient("mongodb://Admin:TeamTrees11!@ds351455.mlab.com:51455/teamtrees")
+
 
 # Function which returns bot menu as a string to be displayed on command line
 def showMenu():
@@ -5,9 +8,20 @@ def showMenu():
     return s
 
 # Function which returns a string containing a list of all the resources available to the student
-def showResources():
+def showResources(mongoValue):
     # Functions that displays a list of resources available submitted by the admin. Reads from database later
-    resources = {'Resource1': 'https://github.com/4rsalan/WebexBot/blob/master/flaskTest.py',
-                 'Resource2': 'https://github.com/4rsalan/WebexBot/blob/master/flaskTest.py'}
-
+    #resources = {'Resource1': 'https://github.com/4rsalan/WebexBot/blob/master/flaskTest.py','Resource2': 'https://github.com/4rsalan/WebexBot/blob/master/flaskTest.py'}
+    resources = mongoValue['teamtrees']['resources']
     return str(resources)
+
+
+def addResource(mongoValue, resource_name, resource_value):
+    mongoValue["teamtrees"]["resources"].insert_one({resource_name: resource_value})
+
+
+
+def showHelp():
+    s = ""
+    return s
+
+addResource(myclient, "Hello", "My name is arsalan")
